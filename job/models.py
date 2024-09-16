813598fa6528ba2901b8c4de5445637226cb66de
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-16 22:30:40 +0300

    create basic model for Job app

--- job/models.py
+++ job/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    location = models.
+    
+
+    # class Meta:
+    #     verbose_name = _("")
+    #     verbose_name_plural = _("s")
+
+    # def __str__(self):
+    #     return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})

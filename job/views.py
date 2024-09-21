804700e2c343c74bf7a2248bf242a4c1b3314e0d
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-21 15:19:29 +0300

    add render templet to each endpoint to test it

--- job/views.py
+++ job/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    
+    return render(request ,'job/jobs.html',{'job_list' : job_list})
 
 
 def job_detail(request, id):
-    pass
+    job_detail = Job.objects.get(id = id)
+    
+    return render(request ,'job/job_detail.html',{'job_detail' : job_detail})
